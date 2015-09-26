1e2c1b82da80810879a2bcd08573626423ef2f5e
Justas T <j.trimailovas@gmail.com>
2015-09-26 11:58:25 +0300

    Prepend zeroes to single minutes
    e.g. "1h 6min" turn into "1h 06min"

--- timeflow/helpers.py
+++ timeflow/helpers.py
@@ -146,10 +146,10 @@ def create_report(report_dict):
         for log in project_report:
             total_seconds += project_report[log]
             hr, mn = get_time(project_report[log])
-            report += "    {hours}h {minutes}min: {log}\n".format(
-                log=log,
+            report += "    {hours}h {minutes:02}min: {log}\n".format(
                 hours=hr,
                 minutes=mn,
+                log=log
             )
         hr, mn = get_time(total_seconds)
         report += "Total: {hours}h {minutes:02}min\n".format(
