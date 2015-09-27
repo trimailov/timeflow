3558a6b9b74ff1b8f9015f4e50bf5abe1d091c92
Justas T <j.trimailovas@gmail.com>
2015-09-27 18:48:40 +0300

    Update setup.py with more info

--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@ from setuptools import setup
 
 setup(
     name='timeflow',
+    packages=['timeflow'],
     version='0.1',
+    description='Small CLI time logger',
+    author='Justas Trimailovas',
+    author_email='j.trimailvoas@gmail.com',
+    url='https://github.com/trimailov/timeflow',
+    keywords=['timelogger', 'logging', 'timetracker', 'tracker'],
     py_modules=['timeflow'],
     entry_points='''
         [console_scripts]
