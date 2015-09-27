9424a3385f1330cffbf9fa084e58ef107f73a4b8
Justas T <j.trimailovas@gmail.com>
2015-09-27 19:01:33 +0300

    Add long description from README.rst

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
+import os
 from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='timeflow',
     packages=['timeflow'],
     version='0.1',
     description='Small CLI time logger',
+
     author='Justas Trimailovas',
     author_email='j.trimailvoas@gmail.com',
+
     url='https://github.com/trimailov/timeflow',
     keywords=['timelogger', 'logging', 'timetracker', 'tracker'],
+
+    long_description=read('README.rst'),
+
     py_modules=['timeflow'],
     entry_points='''
         [console_scripts]
