e2ecea4f3cd8432935a6d823c270f551adea4e62
Justas Trimailovas <j.trimailovas@gmail.com>
2017-01-30 14:57:30 +0200

    Create user settings
    And create user settings file if it does not exist

--- timeflow/main.py
+++ timeflow/main.py
@@ -1,5 +1,20 @@
+import os
+
 from timeflow import cli
+from timeflow.settings import Settings
 
 
 def main():
+    settings = Settings()
+    config_file = settings.get_config_file()
+
+    # create config file directory if it does not exist
+    if not os.path.exists(os.path.dirname(config_file)):
+        os.makedirs(os.path.dirname(config_file))
+
+    # create settings config file if it does not exists and save settings there
+    if not os.path.exists(config_file):
+        open(config_file, 'a').close()
+        settings.save()
+        print('Settings file at {} was created!'.format(config_file))
     cli.cli()
---
+++ timeflow/settings.py
@@ -0,0 +1,44 @@
+import os
+
+from configparser import ConfigParser
+
+
+class Settings():
+    activity_email = "activity@example.com"
+    email_address = "jondoe@example.com"
+    email_user = "jondoe"
+    email_password = "mypassword"
+    smtp_server = "smtp.myserver.com"
+    smtp_port = 25
+
+    def config(self):
+        config = ConfigParser()
+        config["timeflow"] = {
+            "activity_email": self.activity_email,
+            "email_address": self.email_address,
+            "email_user": self.email_user,
+            "email_password": self.email_password,
+            "smtp_server": self.smtp_server,
+            "smtp_port": self.smtp_port,
+        }
+        return config
+
+    def get_config_file(self):
+        return os.path.join(os.path.expanduser('~'), '.config', 'timeflow', 'settings.ini')
+
+    def load(self):
+        config_file = self.get_config_file()
+        config = self.config()
+        config.read(config_file)
+        self.activity_email = config['timeflow']['activity_email']
+        self.email_address = config['timeflow']['email_address']
+        self.email_user = config['timeflow']['email_user']
+        self.email_password = config['timeflow']['email_password']
+        self.smtp_server = config['timeflow']['smtp_server']
+        self.smtp_port = config['timeflow']['smtp_port']
+
+    def save(self):
+        config_file = self.get_config_file()
+        config = self.config()
+        with open(config_file, 'w') as f:
+            config.write(f)
